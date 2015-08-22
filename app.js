844521fed77b6d9810691fbc5df86f70e8fd3481
Joe Kent <thedeadlybutter@gmail.com>
2015-08-22 01:02:02 -0400

    Adds position sync

--- app.js
+++ app.js
@@ -13,13 +13,58 @@ app.use(sassMiddleware({
 
 app.use(express.static('public'));
 
-app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
-});
+var uuid = require('uuid');
 
-var server = app.listen(process.env.PORT || 3000, function () {
+var server = require('http').Server(app);
+server.listen(process.env.PORT || 3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
   console.log('Example app listening at http://%s:%s', host, port);
+});
+var io = require('socket.io')(server);
+
+// GAME VARS
+
+var monsters = {};
+var monsterSpeed = 2;
+
+// END GAME VARS
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+  var id = uuid.v4();
+  var monster = {
+    id: id,
+    x: 10,
+    y: 20,
+    type: 'gargant',
+    direction: 'up'
+  }
+  monsters[id] = monster;
+  socket['monster_id'] = id;
+  socket.emit('start', {monster: monster, info: {speed: monsterSpeed}});
+
+  socket.on('move', function (data) {
+    if(data.id == undefined || data.x == undefined || data.y == undefined) {
+      return;
+    }
+    var monster = monsters[data.id];
+    if(monster.x + monsterSpeed == data.x || monster.x - monsterSpeed == data.x || monster.x == data.x) {
+      if(monster.y + monsterSpeed == data.y || monster.y - monsterSpeed == data.y || monster.y == data.y) {
+        monster.x = data.x;
+        monster.y = data.y;
+        socket.broadcast.emit('update-pos', monster);
+      }
+    }
+  });
+
+  socket.on('disconnect', function() {
+    socket.broadcast.emit('player-disconnect', socket.monster_id);
+    delete monsters[socket.monster_id];
+  });
+
 });
